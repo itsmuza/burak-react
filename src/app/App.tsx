f6d1244666e30ef114a3085479968fad4ee21799
Muzaffar <itsmuuza@icloud.com>
2026-06-25 16:31:34 +0900

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../css/app.css";
+import { Box, Button, Stack, Container, Typography } from "@mui/material";
+import { RippleBadge } from "./MaterialTheme/styled";
+
+function App() {
+  return (
+    <Container sx={{ background: "orange" }}>
+      <Stack direction="column">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h4">
+            Create React App on TypeScript with REDUX
+          </Typography>
+        </Box>
+        <Box>
+          <RippleBadge badgeContent={4}>
+            <Button variant="contained">Contained</Button>
+          </RippleBadge>
+        </Box>
+      </Stack>
+    </Container>
+  );
+}
+
+export default App;
