00db69e5ab046bfeb1ba4d6050c87e89070f7664
Muzaffar <itsmuuza@icloud.com>
2026-06-26 15:50:30 +0900

    feat: organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,21 @@
 import React from "react";
 import "../css/app.css";
 import { Box, Button, Stack, Container, Typography } from "@mui/material";
-import { Link, Route, Switch } from "react-router-dom";
-import { HomePage } from "./MaterialTheme/screens/homePage";
-import { ProductsPage } from "./MaterialTheme/screens/productsPage";
-import { OrdersPage } from "./MaterialTheme/screens/ordersPage";
-import { UserPage } from "./MaterialTheme/screens/userPage";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
+import { HomePage } from "./screens/homePage";
+import { ProductsPage } from "./screens/productsPage";
+import { OrdersPage } from "./screens/ordersPage";
+import { UserPage } from "./screens/userPage";
+import { HomeNavbar } from "./components/headers/HomeNavbar";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import { Footer } from "./components/footer";
 
 function App() {
-  return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">HomePage</Link>
-        </li>
-        <li>
-          <Link to="/products">ProductsPage</Link>
-        </li>
-        <li>
-          <Link to="/orders">OrdersPage</Link>
-        </li>
-        <li>
-          <Link to="/member-page">UserPage</Link>
-        </li>
-      </ul>
+  const location = useLocation();
 
+  return (
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
       <Switch>
         <Route path="/products">
           <ProductsPage />
@@ -39,7 +30,9 @@ function App() {
           <HomePage />
         </Route>
       </Switch>
-    </div>
+
+      <Footer />
+    </>
   );
 }
 
