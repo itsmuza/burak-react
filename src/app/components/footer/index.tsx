a76f4ea28d07ff38ca7b8aad07ac38492fe2e1eb
Muzaffar <itsmuuza@icloud.com>
2026-06-29 17:43:31 +0900

    fix: organize homepage related sectional component

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -37,7 +37,7 @@ export default function Footer() {
           <Stack sx={{ ml: "288px" }} direction={"row"}>
             <Stack>
               <Box>
-                <Box className={"foot-category-title"}>Bo'limlar</Box>
+                <Box className={"foot-category-title"}>Sections</Box>
                 <Box className={"foot-category-link"}>
                   <Link to="/">Home</Link>
                   <Link to="/products">Products</Link>
